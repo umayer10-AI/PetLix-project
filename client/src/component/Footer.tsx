0c662d2fb0fd9de04f1ada6db3b640b62c4b4a31
umayer10-AI <mdumayer10@gmail.com>
2026-05-29 19:22:23 +0600

    add

--- client/src/component/Footer.tsx
+++ client/src/component/Footer.tsx
@@ -7,7 +7,7 @@ import { RiInstagramFill } from 'react-icons/ri';
 const Footer = () => {
   return (
     <footer className="w-full bg-[#0f1113] text-white mt-10">
-      <div className="max-w-7xl mx-auto px-6 py-10 grid md:grid-cols-3 gap-10">
+      <div className="max-w-[80%] mx-auto px-6 py-10 grid md:grid-cols-3 gap-10">
 
         <div>
           <h2 className="text-2xl font-bold">
@@ -18,35 +18,39 @@ const Footer = () => {
           </p>
         </div>
 
-        <div>
-          <h3 className="text-lg font-semibold mb-3">Contact</h3>
+        <div className='flex justify-center'>
+            <div>
+                <h3 className="text-lg font-semibold mb-3">Contact</h3>
 
-          <div className="space-y-2 text-gray-400">
-            <p className="flex items-center gap-2">
-              <Mail size={16} /> support@petlix.com
-            </p>
-            <p className="flex items-center gap-2">
-              <Phone size={16} /> +880 1234-567890
-            </p>
-          </div>
+                <div className="space-y-2 text-gray-400">
+                    <p className="flex items-center gap-2">
+                    <Mail size={16} /> support@petlix.com
+                    </p>
+                    <p className="flex items-center gap-2">
+                    <Phone size={16} /> +880 1234-567890
+                    </p>
+                </div>
+            </div>
         </div>
 
-        <div>
-          <h3 className="text-lg font-semibold mb-3">Follow Us</h3>
+        <div className='flex justify-center'>
+            <div>
+            <h3 className="text-lg font-semibold mb-3">Follow Us</h3>
 
-          <div className="flex gap-4">
-            <Link href="#" className="hover:text-red-500 transition">
-              <FaFacebook />
-            </Link>
+            <div className="flex gap-4">
+                <Link href="#" className="hover:text-red-500 text-2xl transition">
+                <FaFacebook />
+                </Link>
 
-            <Link href="#" className="hover:text-red-500 transition">
-              <RiInstagramFill />
-            </Link>
+                <Link href="#" className="hover:text-red-500 text-2xl transition">
+                <RiInstagramFill />
+                </Link>
 
-            <Link href="#" className="hover:text-red-500 transition">
-              <FaTwitter />
-            </Link>
-          </div>
+                <Link href="#" className="hover:text-red-500 text-2xl transition">
+                <FaTwitter />
+                </Link>
+            </div>
+            </div>
         </div>
       </div>
 
