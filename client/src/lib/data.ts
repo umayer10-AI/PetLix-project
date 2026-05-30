8a1566f30b742d9558205ecd3ee2c663ff5cbda2
umayer10-AI <mdumayer10@gmail.com>
2026-05-30 15:33:37 +0600

    add

--- client/src/lib/data.ts
+++ client/src/lib/data.ts
@@ -2,3 +2,8 @@ export const UserData = async() => {
     const res = await fetch(`http://localhost:5000/user`)
     return res.json()
 }
+
+export const UserIDData = async (id) => {
+    const res = await fetch(`http://localhost:5000/user/${id}`)
+    return res.json()
+}
