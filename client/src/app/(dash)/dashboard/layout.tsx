7dc5802016ef5cd7186da7fd5801375f8c1ef8dd
umayer10-AI <mdumayer10@gmail.com>
2026-05-30 16:51:44 +0600

    add

--- client/src/app/(dash)/dashboard/layout.tsx
+++ client/src/app/(dash)/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar2 from '@/component/Navbar2';
+import SideBar from '@/component/SideBar';
 import React, { ReactNode } from 'react';
 
 interface LayoutProps {
@@ -9,8 +10,11 @@ const layout = ({children}: LayoutProps) => {
     return (
         <div>
             <Navbar2></Navbar2>
-            <div>
-                {children}
+            <div className='flex'>
+                <SideBar></SideBar>
+                <div className='w-full'>
+                    {children}
+                </div>
             </div>
         </div>
     );
---
+++ client/src/component/SideBar.tsx
@@ -0,0 +1,41 @@
+'use client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const links = [
+  { name: 'My Request', href: '/dashboard/request' },
+  { name: 'Add Pet', href: '/dashboard/add' },
+  { name: 'My Listings', href: '/dashboard' },
+];
+
+const SideBar = () => {
+    const pathname = usePathname();
+
+  return (
+    <aside className="w-64 h-screen border-r bg-[#0f1113] p-4">
+      <h1 className="text-xl font-bold mb-6">Dashboard</h1>
+
+      <nav className="flex flex-col gap-2">
+        {links.map((link) => {
+          const isActive = pathname === link.href;
+
+          return (
+            <Link
+              key={link.href}
+              href={link.href}
+              className={`px-4 py-2 rounded-md font-medium transition-all ${
+                isActive
+                  ? 'bg-gradient-to-r from-red-700 to-[#220b34] text-white'
+                  : 'text-gray-700 hover:bg-gray-100'
+              }`}
+            >
+              {link.name}
+            </Link>
+          );
+        })}
+      </nav>
+    </aside>
+  );
+};
+
+export default SideBar;
